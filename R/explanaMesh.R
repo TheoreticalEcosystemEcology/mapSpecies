905910a37ae243dc91bea45692475a1a9685d661
Guillaume Blanchet <guiblanchet3@gmail.com>
2019-04-01 16:28:45 -0400

    fine tuned correction to the code and addition to the help file

--- R/explanaMesh.R
+++ R/explanaMesh.R
@@ -12,6 +12,8 @@
 #' 
 #' The time it takes to run this function is directly related to the number of vertex (points) in the mesh; more specifically the ones outside the region of interest.
 #' 
+#' Also, this function checks the projections of \code{sp}, \code{mesh} and \code{X} to make sure they match. Note that if all projections are NAs, the function will run but warnings messages will be printed on screen related to this. 
+#' 
 #' @importFrom sp SpatialPoints
 #' @importFrom raster crs
 #' @importFrom raster raster
@@ -27,6 +29,34 @@
 #'
 #' @export
 explanaMesh <- function(sp, mesh, X, verbose = TRUE){
+  #=================
+  # Check projection
+  #=================
+  # Extract projection of objects
+  projSp <- crs(sp)@projargs
+  projX <- crs(X)@projargs
+  
+  if(is.na(crs(mesh))){
+    projMesh <- NA
+  }else{
+    projMesh <- mesh$crs@projargs
+  }
+  
+  proj <- c(projSp, projMesh, projX)
+  
+  # Check if all projections are NAs
+  if(all(is.na(proj))){
+    warning("All objects have NA as projection, make sure this is right")
+  }else{
+    if(any(is.na(proj))){
+      stop("At least one object has NA as projection")
+    }else{
+      if(!((proj[1] == proj[2]) & (proj[1] == proj[3]) & (proj[2] == proj[3]))){
+        stop("The projection for at least one of the spatial object is different from the others")
+      }
+    }
+  }
+
   #============================================================
   # If the edges are not within the raster boundaries associate
   # them with a close by pixel
@@ -44,30 +74,28 @@ explanaMesh <- function(sp, mesh, X, verbose = TRUE){
   Quad <- split(as.data.frame(pts), quad)
   
   # Find new coordinates (by quadrants)
+  
   new.pts <- lapply(names(Quad), function(x) {
     switch(x, 
-           '0' = xyFromCell(X, ncell(X) - ncol(X) + 1)[rep(1, nrow(Quad[[x]])), ],
-           '1' = xyFromCell(X, cellFromXY(X, cbind(xmin(X), Quad[[x]][, 2]))),
-           '2' = xyFromCell(X, 1)[rep(1, nrow(Quad[[x]])), ],
-           '3' = xyFromCell(X, cellFromXY(X, cbind(Quad[[x]][, 1], ymin(X)))),
-           '4' = {
-                  xy <- as.matrix(Quad[[x]])
-                  dimnames(xy) <- list(NULL, c('x', 'y'))
-                  xy
-                 },
-           '5' = xyFromCell(X, cellFromXY(X, cbind(Quad[[x]][, 1], ymax(X)))),
-           '6' = xyFromCell(X, ncell(X))[rep(1, nrow(Quad[[x]])), ],
-           '7' = xyFromCell(X, cellFromXY(X, cbind(xmax(X), Quad[[x]][, 2]))),
-           '8' = xyFromCell(X, ncol(X))[rep(1, nrow(Quad[[x]])), ]
+           '0' = matrix(xyFromCell(X, ncell(X) - ncol(X) + 1)[rep(1, nrow(Quad[[x]])), ], ncol = 2),
+           '1' = matrix(xyFromCell(X, cellFromXY(X, cbind(xmin(X), Quad[[x]][, 2]))), ncol = 2),
+           '2' = matrix(xyFromCell(X, 1)[rep(1, nrow(Quad[[x]])), ], ncol = 2),
+           '3' = matrix(xyFromCell(X, cellFromXY(X, cbind(Quad[[x]][, 1], ymin(X)))), ncol = 2),
+           '4' = matrix(unlist(Quad[[x]]), ncol = 2),
+           '5' = matrix(xyFromCell(X, cellFromXY(X, cbind(Quad[[x]][, 1], ymax(X)))), ncol = 2),
+           '6' = matrix(xyFromCell(X, ncell(X))[rep(1, nrow(Quad[[x]])), ], ncol = 2),
+           '7' = matrix(xyFromCell(X, cellFromXY(X, cbind(xmax(X), Quad[[x]][, 2]))), ncol = 2),
+           '8' = matrix(xyFromCell(X, ncol(X))[rep(1, nrow(Quad[[x]])), ], ncol = 2)
            )
         }
       )
   
   # Reorganize to a data.frame 
   new.pts <- unsplit(mapply(function(x, y) {
-    row.names(x) <- row.names(y)
-    as.data.frame(x)
-  }, new.pts, Quad, SIMPLIFY=FALSE), quad)
+                            row.names(x) <- row.names(y)
+                            as.data.frame(x)
+                           }, 
+                     new.pts, Quad, SIMPLIFY=FALSE), quad)
   
   # Construct SpatialPoints from mesh edges
   loc <- SpatialPoints(coords = new.pts,
@@ -112,7 +140,7 @@ explanaMesh <- function(sp, mesh, X, verbose = TRUE){
     
       if(verbose){
         if(any(i == round(seq(0, nlocValNA, length = 7)[-c(1,7)]))){
-          print(paste(i," out of ",nlocValNA,"elements considered",
+          print(paste(i," out of ",nlocValNA," edges with NAs considered",
                       sep = ""))
         }
       }
@@ -125,3 +153,4 @@ explanaMesh <- function(sp, mesh, X, verbose = TRUE){
   
   return(results)
 }
+
