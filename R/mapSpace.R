4b307b007aeaf75460c8c79ccb9a4dff387bd9b5
Guillaume Blanchet <guiblanchet3@gmail.com>
2019-04-02 15:10:28 -0400

    Renamed mapSpatial to mapSpace for consistency and adapted it to the new structure of uniSpace

---
+++ R/mapSpace.R
@@ -0,0 +1,49 @@
+#' @title Construct species distribution maps models
+#' 
+#' @description Constructs mean, standard deviation, and quantile (0.025, 0.5 and 0.975) maps for models calculated using \code{\link{uniSpace}} and \code{\link{ppSpace}} 
+#' 
+#' @param modelSpace An object of class \code{ppSpace} or of class \code{uniSpace}.
+#' @param dims A vector of length 2 defining the number of pixels to use as rows and columns to define the map.
+#' @param type Either "mean", "sd", "0.025quant", "0.5quant", "0.975quant" or "mode". Defines the map to be drawn.
+#' @param sp A spatial polygon to isolate the region of interest. If none is given, a map is drawn for the entire region covered by the mesh. 
+#' 
+#' @importFrom INLA inla.mesh.projector
+#' @importFrom INLA inla.mesh.project
+#' @importFrom INLA inla.stack.index
+#' @importFrom raster raster
+#' @importFrom raster mask
+#' 
+#' @export
+#' 
+mapSpace <- function(modelSpace, dims, 
+                       type = c("mean", "sd", "0.025quant", 
+                                "0.5quant", "0.975quant",
+                                "mode"), sp = NULL){
+  ### General check
+  if(length(type) > 1){
+    stop("Only one type should be defined")
+  }
+  
+  ### Define map basis
+  mapBasis <- inla.mesh.projector(attributes(modelSpace)$mesh, dims = dims)
+  
+  ### Find the mesh edges on which predictions should be made
+  ID <- inla.stack.index(attributes(modelSpace)$Stack, tag="pred")$data
+  
+  ### Calculate prediction
+  mapPred <- inla.mesh.project(mapBasis, 
+                               modelSpace$summary.fitted.values[[type]][ID])
+  
+  ### Transform map into a raster
+  mapRaster <- raster(t(mapPred[,ncol(mapPred):1]),
+                      xmn = min(mapBasis$x), xmx = max(mapBasis$x), 
+                      ymn = min(mapBasis$y), ymx = max(mapBasis$y))
+  
+  ### Isolate region of interest
+  if(!is.null(sp)){
+    mapRaster <- mask(mapRaster, sp)
+  }
+  
+  ### Return
+  return(mapRaster)
+}
